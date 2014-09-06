c8425b2ad55fe2cc0811ac847c8caae10846878d
Frank Mueller <mue@tideland.biz>
2014-09-06 22:23:03 +0200

    gocn/*: adjusted versions

--- v3/testsupport/doc.go
+++ v3/testsupport/doc.go
@@ -24,7 +24,7 @@ import (
 
 // PackageVersion returns the version of the version package.
 func PackageVersion() version.Version {
-	return version.New(3, 0, 0)
+	return version.New(3, 0, 0, "alpha", "2014-09-06")
 }
 
 // EOF
